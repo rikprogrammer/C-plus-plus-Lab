5c4cfdde9b8cb78f3068ae17378c0194c91355d4
Anubrato Mukherjee <81558380+rikprogrammer@users.noreply.github.com>
2026-09-22 17:59:48 +0530

    Update Constructor_Destructor.cpp

--- Anubrato_C++ Lab/Constructor_Destructor.cpp
+++ Anubrato_C++ Lab/Constructor_Destructor.cpp
@@ -1,56 +1,65 @@
 #include <iostream>
 using namespace std;
+
 class Shape
 {
-  private:
-          double leng, brdt, rad;
-  
-  public:
-  		  Shape(double l, double b)
-		  {
-		    leng = l;
-			brdt = b;	
-		  }
-		  
-		  Shape(double r)
-		  {
-		    rad = r;	
-		  }
-		  
-		  double rectanglePerimeter()
-		  {
-		    return (2*(leng+brdt));
-		  }
-		  
-		  double circlePerimeter()
-		  {
-		    return (2*3.14*rad);	
-		  }
-		  
-		  ~Shape()
-		  {
-		    cout << "Destructor called\n";
-		  }		
+private:
+    double leng, brdt, rad;
+
+public:
+    Shape(double l, double b)
+    {
+        leng = l;
+        brdt = b;
+        rad = 0;
+    }
+
+    Shape(double r)
+    {
+        leng = 0;
+        brdt = 0;
+        rad = r;
+    }
+
+    double rectanglePerimeter()
+    {
+        return 2 * (leng + brdt);
+    }
+
+    double circlePerimeter()
+    {
+        const double PI = 3.141592653589793;
+        return 2 * PI * rad;
+    }
+
+    ~Shape()
+    {
+        cout << "Destructor called" << endl;
+    }
 };
 
 int main()
 {
-  float l, b, r;
-  
-  cout << "Enter the length of rectangle:- ";
-  cin >> l;
-  
-  cout << "Enter the breadth of rectangle:- ";
-  cin >> b;
-  
-  Shape rect(l, b);
-  cout << "Rectangle perimeter:- " << rect.rectanglePerimeter() << endl;
-  
-  cout << "Enter the radius of circle:- ";
-  cin >> r;	
-  
-  Shape circ(r);
-  cout << "Circle perimeter:- " << circ.circlePerimeter();
-  
-  return 0;
+    double l, b, r;
+
+    cout << "Enter the length of rectangle: ";
+    cin >> l;
+
+    cout << "Enter the breadth of rectangle: ";
+    cin >> b;
+
+    Shape rect(l, b);
+
+    cout << "Rectangle perimeter: "
+         << rect.rectanglePerimeter() << endl;
+
+    cout << "Enter the radius of circle: ";
+    cin >> r;
+
+    Shape circ(r);
+
+    cout << "Circle perimeter: "
+         << circ.circlePerimeter() << endl;
+
+    return 0;
 }
